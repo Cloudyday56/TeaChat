7f6a3f6156a036965d205e2de1f498e3a838b8d6
Cloudyday56 <Rchen78900@gmail.com>
2025-06-29 12:35:53 -0400

    (deployment) App ready!

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 import { connectDB } from "./lib/db.js";
 
@@ -15,6 +16,8 @@ dotenv.config(); //load environment variables from .env file
 
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve(); //get the current directory name
+
 // app.use(express.json()); //so that we can parse JSON data from the request body
 app.use(cookieParser()); //so that we can parse cookies from the request
 app.use(cors({
@@ -25,6 +28,14 @@ app.use(express.json({ limit: '2mb' })); //limit the JSON data to 2mb
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// production (putting both frontend and backend in the same server)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))); //serve static files from the frontend build directory
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html")); //send the index.html file for any other route
+    });
+}
 
 server.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
